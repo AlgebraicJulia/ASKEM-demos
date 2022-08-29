6a829c9e87eccb63f9831fdb765c0e3548ac7493
James P. Fairbanks <fairbanksj@ufl.edu>
2022-08-29 18:14:35 -0400

    add MTK integration for Petri Nets

---
+++ MTK/petri.jl
@@ -0,0 +1,95 @@
+using ModelingToolkit
+using AlgebraicPetri
+using AlgebraicPetri.Epidemiology
+using AlgebraicPetri.BilayerNetworks
+
+using Catlab
+using Catlab.CategoricalAlgebra
+using Catlab.Graphics
+import Catlab.CategoricalAlgebra: migrate!
+using Catlab.WiringDiagrams
+using Catlab.Programs.RelationalPrograms
+display_uwd(ex) = to_graphviz(ex, box_labels=:name, junction_labels=:variable, edge_attrs=Dict(:len=>".75"));
+
+sir = @relation (s,i,r) begin
+    infection(s,i)
+    recovery(i,r)
+end
+display_uwd(sir)
+
+psir = apex(oapply_epi(sir))
+psir
+Graph(psir)
+
+bnsir = LabelledBilayerNetwork()
+migrate!(bnsir, psir)
+bnsir
+to_graphviz(bnsir)
+
+function compile(bn::Union{AbstractLabelledBilayerNetwork, AbstractBilayerNetwork})
+  varstmt = :(@variables t)
+  append!(varstmt.args, bnsir[:variable])
+
+  paramstmt = :(@parameters)
+  append!(paramstmt.args, bnsir[:parameter])
+
+  diffstmt = :(D = Differential(t))
+
+  ϕs = map(parts(bn, :Box)) do b
+    vars = map(incident(bn, b,:call)) do i
+      j = bn[i, :arg]
+      return bn[j, :variable]
+    end
+    p = :(*($(bn[b, :parameter])))
+    append!(p.args, vars)
+    return :($(Symbol("ϕ$b")) = $p)
+  end
+
+  infs = map(parts(bn, :Qout)) do tv
+    vars = map(incident(bn, tv, :infusion)) do wa
+      j = bn[wa, :influx]
+      return Symbol("ϕ$j")
+    end
+    p = :(+())
+    append!(p.args, vars)
+
+    # same for the outfluxes
+    vars = map(incident(bn, tv, :effusion)) do wn
+      j = bn[wn, :efflux]
+      return :(- $(Symbol("ϕ$j")))
+    end
+    append!(p.args, vars)
+    return p
+  end
+
+  zparts = zip(bn[:tanvar], infs)
+
+  eqns = [:(D($tanvar) ~ $rhs) for (tanvar, rhs) in zparts]
+  eq = :([])
+  append!(eq.args, eqns)
+  eqnstmt = :(eqs = $eq)
+
+
+  return quote
+    $varstmt
+    $paramstmt
+    $diffstmt
+    $(ϕs...)
+    $eqnstmt
+    return ODESystem(eqs, t, name=:PetriNet)
+  end
+
+end
+
+fex = compile(bnsir)
+
+quote
+  #= /Users/fairbanksj/github/AlgebraicJulia/ASKEM-demos/MTK/petri.jl:39 =# 
+  @variables t S I R
+  @parameters inf rec
+  D = Differential(t)
+  ϕ1 = inf * S * I
+  ϕ2 = rec * I
+  eqs = [D(S) ~ +(-ϕ1), D(I) ~ ϕ1 + ϕ1 + -ϕ1 + -ϕ2, D(R) ~ +ϕ2]
+  return ODESystem(eqs, t, name = :PetriNet)
+end
