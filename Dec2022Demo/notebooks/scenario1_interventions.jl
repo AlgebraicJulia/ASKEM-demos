34866e827eff35d7d82e75cf999724c67225d079
Owen Lynch <root@owenlynch.org>
2022-12-06 14:47:36 -0800

    addded interactive pluto notebook

--- Dec2022Demo/notebooks/scenario1_interventions.jl
+++ Dec2022Demo/notebooks/scenario1_interventions.jl
@@ -37,52 +37,32 @@ end
 
 # ╔═╡ 72daa7df-e8c2-4913-b445-9fbbd8b885ec
 md"""
-Policies:
-- Implement restrictions at fixed date
-- Implement restrictions after cases are above a given number
-
-Potential real world challenges (might not get to these):
-- Have to give people time to prepare for lockdown
-- Reported cases lag behind actual cases
-- Effects of lockdown on transmission unknown
-
-Problem:
-Make a toolkit for evaluating different policies in different scenarios. This should include explorative evaluation and probabilistic evaluation.
-
-TODO:
-Figure appropriate scaling for all variables to correspond to actual situation.
+This notebook allows you to choose between several epidemiological Petri nets and several different policies, and then simulate what the effect of those policies would be.
 """
 
-# ╔═╡ b003c833-8d56-4a7d-af3d-7007328368ef
-sir_uwd = @relation (s, i, r) where (s, i, r) begin
-    infection(s, i)
-    recovery(i, r)
-end;
-
-# ╔═╡ ede2718c-489d-42df-a39f-fdec4646a9c1
-sir_petri = apex(oapply_epi(sir_uwd));
-
-# ╔═╡ 9b02c29f-012a-4b90-9b36-97871f9b0075
+# ╔═╡ 82c9f255-41c2-4933-b31b-6558f9243495
 md"""
-TODO: Should have a selection of different Petri nets, SIR, SEIR, etc.
-
-For each Petri net, we should expose the variables of interest, i.e. case counts and hospitalization, as expressions in the ModelingToolkit variables.
+First, select a Petri net.
 """
+
+# ╔═╡ 62b804b2-d355-42fb-8e4c-f5eb9eed1696
+petri_options = [
+	EpidemiologicalPetriNets.SIR => "SIR",
+	EpidemiologicalPetriNets.SIRD => "SIRD",
+	EpidemiologicalPetriNets.SEIR => "SEIR",
+	EpidemiologicalPetriNets.SEIRD => "SEIRD"
+];
+
+# ╔═╡ 9ea0277f-ed20-44e8-bbf2-16e8815ee65b
+@bind petri Select(petri_options)
 
 # ╔═╡ f62b4be9-9da4-41c9-a6e4-7da5653403e7
-@named sir = ASKEM.PetriMTK.rate_equation(sir_petri)
+@named model = ASKEM.PetriMTK.rate_equation(petri)
 
-# ╔═╡ e821e0fe-16fe-4320-8ea7-2d4dd91f3cb9
-@bind intervention_params Sliders(
-	"Intervention Parameters",
-	["case_threshhold" => 0:5.0:500, 
-	 "lockdown_effect" => 0:0.01:1.0,
-	 "time_of_lockdown" => 0:0.01:1.0
-	]
-)
-
-# ╔═╡ e542286b-bfa8-43d9-b150-6476d5580825
-(case_threshold, lockdown_effect, time_of_lockdown) = intervention_params
+# ╔═╡ a437c4ce-f5ae-43bb-ac44-c14141fe368a
+md"""
+Then choose a policy. The two choices are a lockdown at a fixed, predefined time, and a lockdown triggered by a certain number of cases.
+"""
 
 # ╔═╡ 620e4c0e-b5af-412b-8a96-b758cb5387f9
 @bind policy_choice Select([
@@ -90,23 +70,40 @@ For each Petri net, we should expose the variables of interest, i.e. case counts
 	:time => "Time Triggered Lockdown"
 ])
 
+# ╔═╡ f1f9107d-836f-4807-98d0-08a96e9b6a5e
+md"""
+The parameters for the policy can then be set with the following sliders.
+"""
+
+# ╔═╡ e821e0fe-16fe-4320-8ea7-2d4dd91f3cb9
+@bind intervention_params Sliders(
+	"Intervention Parameters",
+	["case_threshhold" => 0:0.01:1.0, 
+	 "lockdown_effect" => 0:0.01:1.0,
+	 "time_of_lockdown" => 0:1.0:50.0
+	]
+)
+
+# ╔═╡ e542286b-bfa8-43d9-b150-6476d5580825
+(case_threshold, lockdown_effect, time_of_lockdown) = intervention_params
+
 # ╔═╡ ceda9aac-6e23-4606-9233-d76758d0913f
 policy = Dict(
-	:case => caseload_triggered_lockdown(sir, sir.I, case_threshold, lockdown_effect),
-	:time => time_triggered_lockdown(sir, time_of_lockdown, lockdown_effect)
+	:case => caseload_triggered_lockdown(model, model.I, case_threshold, lockdown_effect),
+	:time => time_triggered_lockdown(model, time_of_lockdown, lockdown_effect)
 )[policy_choice];
 
 # ╔═╡ b895c52e-1592-4905-a38c-7c9d70b92509
-@named sir_intervened = ModelingToolkit.compose(policy, sir);
+@named model_intervened = ModelingToolkit.compose(policy, model);
 
 # ╔═╡ bbc73193-584e-4681-b30f-e6b19438eae7
-@bind init Sliders("Initial Concentrations", ["S" => 0:50.0:1000, "I" => 0:5.0:100, "R" => 0:5.0:100])
+@bind init Sliders("Initial Concentrations", [string(sname(petri, i)) => 0:0.001:1 for i in 1:ns(petri)])
 
 # ╔═╡ c6c820af-1a8c-46c2-827e-547b9ab817c5
-@bind rates Sliders("Rates", ["inf" => 0:0.001:0.1, "rec" => 0:0.1:2.0])
+@bind rates Sliders("Rates", [string(tname(petri, i)) => 0:0.001:1 for i in 1:nt(petri)])
 
 # ╔═╡ fce68171-13a9-4af3-a012-8d8b1ce763c6
-prob = ODEProblem(sir_intervened, [init..., rates...], (0.0, 2.0), []);
+prob = ODEProblem(model_intervened, [init..., rates...], (0.0, 50.0), []);
 
 # ╔═╡ 66f0ce87-2c08-4ad7-838b-56f9ed5029ac
 plot(solve(prob))
@@ -123,17 +120,19 @@ Modeling this will need an agent-based, spatial model, and I expect that we migh
 # ╔═╡ Cell order:
 # ╠═82d23846-7045-11ed-152b-49bacb843ba4
 # ╟─72daa7df-e8c2-4913-b445-9fbbd8b885ec
-# ╠═b003c833-8d56-4a7d-af3d-7007328368ef
-# ╠═ede2718c-489d-42df-a39f-fdec4646a9c1
-# ╟─9b02c29f-012a-4b90-9b36-97871f9b0075
+# ╟─82c9f255-41c2-4933-b31b-6558f9243495
+# ╟─62b804b2-d355-42fb-8e4c-f5eb9eed1696
+# ╠═9ea0277f-ed20-44e8-bbf2-16e8815ee65b
 # ╠═f62b4be9-9da4-41c9-a6e4-7da5653403e7
-# ╟─e821e0fe-16fe-4320-8ea7-2d4dd91f3cb9
-# ╟─e542286b-bfa8-43d9-b150-6476d5580825
+# ╟─a437c4ce-f5ae-43bb-ac44-c14141fe368a
 # ╟─620e4c0e-b5af-412b-8a96-b758cb5387f9
-# ╠═ceda9aac-6e23-4606-9233-d76758d0913f
+# ╟─f1f9107d-836f-4807-98d0-08a96e9b6a5e
+# ╠═e821e0fe-16fe-4320-8ea7-2d4dd91f3cb9
+# ╟─e542286b-bfa8-43d9-b150-6476d5580825
+# ╟─ceda9aac-6e23-4606-9233-d76758d0913f
 # ╠═b895c52e-1592-4905-a38c-7c9d70b92509
-# ╟─bbc73193-584e-4681-b30f-e6b19438eae7
-# ╟─c6c820af-1a8c-46c2-827e-547b9ab817c5
+# ╠═bbc73193-584e-4681-b30f-e6b19438eae7
+# ╠═c6c820af-1a8c-46c2-827e-547b9ab817c5
 # ╠═fce68171-13a9-4af3-a012-8d8b1ce763c6
 # ╠═66f0ce87-2c08-4ad7-838b-56f9ed5029ac
 # ╟─7e16d79d-1d1b-4003-9db2-de8de296a40f
--- lib/src/EpidemiologicalPetriNets.jl
+++ lib/src/EpidemiologicalPetriNets.jl
@@ -1,18 +1,55 @@
 module EpidemiologicalPetriNets
-export SIRD, SIRD_typed
+export SIR, SIRD, SEIR, SEIRD, SVIIvR,
+  SIRD_typed
 
 using ..Ontologies
 
 using AlgebraicPetri
 using Catlab.CategoricalAlgebra
 
-SIRD = LabelledPetriNet([:S, :I, :R, :D],
-    :inf => ((:S,:I) => (:I,:I)),
-    :recover => (:I=>:R),
-    :death => (:I=>:D));
+SIR = LabelledPetriNet(
+  [:S, :I, :R],
+  :inf => ((:S,:I) => (:I,:I)),
+  :recover => (:I=>:R)
+)
 
-SIRD_typed = homomorphism(SIRD, strip_names(infectious_ontology);
-                          initial=(T=[1,2,2],I=[1,2,3,3],O=[1,2,3,3]),
-                          type_components=(Name=x->nothing,))
+SIRD = LabelledPetriNet(
+  [:S, :I, :R, :D],
+  :inf => ((:S,:I) => (:I,:I)),
+  :recover => (:I=>:R),
+  :death => (:I=>:D)
+)
+
+SEIR = LabelledPetriNet(
+  [:S, :E, :I, :R],
+  :inf => ((:S,:I) => (:E,:I)),
+  :sicken => (:E=>:I),
+  :recover => (:I=>:R),
+)
+
+SEIRD = LabelledPetriNet(
+  [:S, :E, :I, :R, :D],
+  :inf => ((:S,:I) => (:E,:I)),
+  :sicken => (:E=>:I),
+  :recover => (:I=>:R),
+  :death => (:I=>:D)
+)
+
+SVIIvR = LabelledPetriNet(
+  [:S, :V, :I, :Iv, :R],
+  :vaccinate => (:S => :V),
+  :si_inf => ((:S,:I) => (:I,:I)),
+  :vi_inf => ((:V,:I) => (:Iv,:I)),
+  :viv_inf => ((:V,:Iv) => (:Iv,:Iv)),
+  :siv_inf => ((:S,:Iv) => (:I,:Iv)),
+  :ivrecover => (:Iv=>:R),
+  :irecover => (:I=>:R)
+)
+
+SIRD_typed = homomorphism(
+  SIRD, strip_names(infectious_ontology);
+  initial=(T=[1,2,2],I=[1,2,3,3],O=[1,2,3,3]),
+  type_components=(Name=x->nothing,)
+)
 
 end
