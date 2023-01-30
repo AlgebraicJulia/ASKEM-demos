5b97387f657626b6ab42ab6009b12df80f1d8b55
p-stokes <patrick.stokes@gmail.com>
2023-01-30 11:42:41 -0500

    Implementation of some models. SIDARTHE json loaded but needs reindexing.

--- HackathonJan2023/eval/models.jl
+++ HackathonJan2023/eval/models.jl
@@ -36,9 +36,22 @@ write_json_acset(sir_age3,"sir_age3.json")
 #*****
 # S2 *
 #*****
+sidarthe = read_json_acset(LabelledPetriNet,"sidarthe.json")
 
+mca_sidarthe_v = mca(sidarthe,sidarthe_v)
+AlgebraicPetri.Graph(mca_sidarthe_v[1])
 
 #*****
 # S3 *
 #*****
 
+vax_lpn = formVax()
+vax_aug_st = vaxAugStates()
+vax_aug = augLabelledPetriNet(vax_lpn,vax_aug_st)
+vax_typed = typeVax(vax_aug,types)
+
+n = 4
+age_aug = makeMultiAge(n)
+age_typed = typeAge(age_aug,types)
+sir_age3 = typed_stratify(sir_typed, age_typed)
+write_json_acset(sir_age3,"sir_age"*string(n)*"".json")
