6abd7ea1ed3a8502dc91b3b5042a4034d1fc8fe1
p-stokes <patrick.stokes@gmail.com>
2023-04-18 14:21:38 -0400

    moved script files to src. some debug code for mca.

--- 202304Demo/src/model_space_explore.jl
+++ 202304Demo/src/model_space_explore.jl
@@ -47,11 +47,38 @@ m3b = PetriNet()
 copy_parts!(m3b,miras[3])
 
 
+m4  = LabelledPetriNet([:S, :I, :R])
+
+
 test = mca(m1,m2)
 boom = []
 for ii in test 
     boom = mca(test,m3)
 end
+
+import AlgebraicPetri.SubACSets: concatmap, exists_mono, one_removed_subobs
+import AlgebraicPetri.SubACSets: size, strip_names
+
+function mca_help(X::ACSet, Y::ACSet)
+    C = acset_schema(X) #X: C → Set
+    getsmaller(Z,c) = map(dom, one_removed_subobs(Z, c))
+    # enumerate all sub-acsets X' ↪ X of the acset X: C → Set obtained by removing one point from Xc for some c ∈ C
+    oneRemovedFromX = concatmap(γ -> getsmaller(X,γ), filter(c -> !isempty(parts(X,c)), objects(C)))
+    # println(oneRemovedFromX)
+    # keep only those X' ↪ X such that there exists mono X' ↪ Y
+    Y_subs          = filter(χ -> exists_mono(χ, Y), oneRemovedFromX)
+    # print("wow")
+    # println(Y_subs)
+    # either terminate or recurse
+    if isempty(Y_subs)
+        # print("wtf")
+      concatmap(χ -> mca_help(χ, Y), oneRemovedFromX)
+    else
+        # println("----- ____________ hi ----- __________")
+      ω = maximum(map(size,Y_subs))
+      filter(y -> size(y) == ω, Y_subs)
+    end
+  end
 
 #****
 
